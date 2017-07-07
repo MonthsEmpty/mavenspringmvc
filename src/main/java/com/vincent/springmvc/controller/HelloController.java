77c7a8efb07b1f8c3691245f7befd6b8dc0c0d8d
MonthsEmpty <615109771@qq.com>
2017-07-07 19:58:58 +0800

    init

--- src/main/java/com/vincent/springmvc/controller/HelloController.java
+++ src/main/java/com/vincent/springmvc/controller/HelloController.java
@@ -1,7 +1,9 @@
 package com.vincent.springmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
@@ -19,7 +21,15 @@ public class HelloController {
     }
 
     @RequestMapping("/second")
-    public String helloView(){
+    public String helloView(@RequestParam String aaa,@RequestParam Integer bbb){
+        System.out.println(aaa);
         return "hello";
+    }
+
+    @RequestMapping("/requestAndResponseTest")
+    @ResponseBody
+    public String requestAndResponseTest(@RequestBody String aaa){
+        System.out.println(aaa);
+        return "requestAndResponseTest";
     }
 }
